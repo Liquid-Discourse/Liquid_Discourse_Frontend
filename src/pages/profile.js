f1c4d3ed6d40e608188f2d5b7e71a513b3ae376a
Sarim Abbas <sarim.stays@gmail.com>
2020-06-09 22:11:20 -0400

    authorize with backend via JWT

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import axios from "axios";
 
 const Profile = () => {
-  const { loading, user } = useAuth0();
+  const { loading, user, getTokenSilently, getIdTokenClaims } = useAuth0();
+  const [profile, setProfile] = useState({});
 
-  console.log(user);
+  useEffect(() => {
+    const getFullProfile = async () => {
+      const token = await getTokenSilently();
+      await console.log(token);
+      const profile = await axios.get("http://localhost:7000/auth/test", {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      await console.log(profile);
+      setProfile(profile);
+    };
+    getFullProfile();
+  }, []);
 
   if (loading || !user) {
     return <div>Loading...</div>;
@@ -12,11 +27,14 @@ const Profile = () => {
 
   return (
     <>
-      <img src={user.picture} alt="Profile" />
-
+      <h1>Via useAuth0 user</h1>
+      <img src={user.picture} alt="Profile" width="100" />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
+      <h1>Via getProfile() from the backend</h1>
+      <img src={profile?.data?.picture} alt="Profile" width="100" />
+      <code>{JSON.stringify(profile, null, 2)}</code>
     </>
   );
 };
