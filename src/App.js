68cef1240f9b484034c58c57d7f53595b0044e5b
Sarim Abbas <sarim.stays@gmail.com>
2020-06-19 19:41:01 -0400

    add tag detail to app routes and document existing routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Settings from "./pages/settings";
 import AddReview from "./pages/add-review";
 import SubPage from "./pages/sub-pages/current-affair-subpage";
 
+import TagDetail from "./pages/detail-pages/tag-detail";
+
 import "./App.css";
 
 import { useAuth0 } from "./react-auth0-spa";
@@ -26,14 +28,24 @@ function App() {
     <div>
       <Router history={history}>
         <Navbar />
+        {/* Main pages */}
         <Route path="/" exact component={Home} />
-        <Route path="/users/:username" exact component={Profile} />
         <Route path="/settings" exact component={Settings} />
-        <Route path="/current-affair/:id" exact component={CurrentAffair} />
-        <Route path="/book/:id" exact component={Book} />
         <Route path="/search-book" exact component={SearchBook} />
-        <Route path="/add-review/:bookId" exact component={AddReview} />
+        {/* Discovery pages */}
+        {/* I recommend we have separate ones for each? e.g. /books, /current-affairs */}
         <Route path="/see-all/:subpage" exact component={SubPage} />
+        {/* Detail pages */}
+        <Route path="/users/:username" exact component={Profile} />
+        <Route path="/book/:id" exact component={Book} />
+        <Route path="/current-affair/:id" exact component={CurrentAffair} />
+        <Route path="/add-review/:bookId" exact component={AddReview} />
+
+        <Route
+          path="/topics/:slug"
+          exact
+          render={(props) => <TagDetail {...props} type="TOPIC" />}
+        />
       </Router>
     </div>
   );
