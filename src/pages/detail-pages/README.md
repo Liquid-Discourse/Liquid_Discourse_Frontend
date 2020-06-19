eb79d56fdd1918ab42b471e2a9ee5180c4b6b58d
Sarim Abbas <sarim.stays@gmail.com>
2020-06-19 19:40:37 -0400

    create tag detail page

---
+++ src/pages/detail-pages/README.md
@@ -0,0 +1,14 @@
+Detail pages show additional information for each database entity. 
+
+Typically, they follow a URL slug format e.g. 
+
+Book pages: 
+1. /books/:bookId
+
+Tag pages: 
+2. /topics/:topicSlug
+3. /current-affairs/:affairSlug
+4. /countries/:countrySlug
+
+
+
---
+++ src/pages/detail-pages/tag-detail.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// We follow a consistent structure for all tag types!
+const TagDetail = (props) => {
+  const tagType = props.type;
+  const tagSlug = props.match.params.slug;
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [tag, setTag] = useState({});
+
+  const getTagFromBackend = async () => {
+    setLoading(true);
+    // get the tag data from the backend
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/tags`, {
+      params: {
+        type: tagType,
+        slug: tagSlug,
+      },
+    });
+    // check if tag exists
+    if (response?.data?.length !== 1) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+    // get first tag in result
+    const tag = response.data[0];
+    setTag(tag);
+    console.log(tag);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getTagFromBackend();
+  }, []);
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
+
+export default TagDetail;
