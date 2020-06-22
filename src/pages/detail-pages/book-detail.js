3f08a181770ca81d0492cab16e2efb003957fb50
Felicia Chang <felicia.chang@yale.edu>
2020-06-22 15:48:42 -0700

    average rating value on book detail page

--- src/pages/detail-pages/book-detail.js
+++ src/pages/detail-pages/book-detail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import useBook from "hooks/book-hook";
 import StarRatings from "react-star-ratings";
@@ -7,6 +7,7 @@ const Name = styled.div`
   font-size: 3.3vh;
   font-family: Montaga;
   margin-bottom: 10px;
+  width: 70%;
 `;
 
 const Subtitle = styled.div`
@@ -64,6 +65,14 @@ const ReviewContainer = styled.div`
 
 const Book = (props) => {
   const { book, addToBookshelf } = useBook(props.match.params.id);
+  const [saved, setSaved] = useState(false);
+
+  const addTo = () => {
+    addToBookshelf();
+    setSaved(true);
+  };
+
+  console.log("book", saved);
 
   return (
     <div style={{ marginRight: "15%", marginLeft: "15%", marginTop: "5%" }}>
@@ -86,6 +95,21 @@ const Book = (props) => {
             ))}
           </Subtitle>
           <br />
+          {book?.reviewCount === 0 ? (
+            <div />
+          ) : (
+            <div>
+              <Subtitle>Average Rating:</Subtitle>
+              <StarRatings
+                rating={book?.averageRatingOutOfFive}
+                starRatedColor="#ffca28"
+                numberOfStars={5}
+                starDimension="18px"
+                starSpacing="3px"
+              />
+            </div>
+          )}
+          <br />
           <Subtitle>ISBN: {book?.isbn}</Subtitle>
           <Subtitle>
             Topics:{" "}
@@ -94,7 +118,7 @@ const Book = (props) => {
             ))}
           </Subtitle>
         </div>
-        <Save onClick={addToBookshelf}>
+        <Save onClick={addTo}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24"
@@ -104,7 +128,7 @@ const Book = (props) => {
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" />
           </svg>
-          {book === {} ? (
+          {saved === false ? (
             <Subtitle>Save Book</Subtitle>
           ) : (
             <Subtitle>Saved!</Subtitle>
