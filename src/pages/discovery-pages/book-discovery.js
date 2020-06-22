1cd092973b03aba344082e0307ca2ceb0d52e97a
Sarim Abbas <sarim.stays@gmail.com>
2020-06-21 23:51:16 -0400

    improve book discovery page with a grid

--- src/pages/discovery-pages/book-discovery.js
+++ src/pages/discovery-pages/book-discovery.js
@@ -3,30 +3,34 @@ import { useHistory } from "react-router-dom";
 import BookCard from "components/book-card";
 import styled from "styled-components";
 import axios from "axios";
+import { Helmet } from "react-helmet";
+import { HorizontalSpacer } from "components/reusable/spacer";
+import { ReactComponent as PlusIcon } from "assets/icons/plus.svg";
 
-const AddCard = styled.div`
+const SubmitCard = styled.div`
   font-family: Poppins;
   box-shadow: 2px 2px 8px rgb(230, 230, 230);
   border-radius: 5px;
-  width: 100%;
-  height: 70px;
-  margin: 1%;
-  padding: 1% 2%;
-  position: relative;
+  padding: 20px 20px;
   display: flex;
   align-items: center;
   &:hover {
     background-color: #ff9e80;
+    cursor: pointer;
   }
 `;
+
 const Title = styled.div`
-  margin-top: 5%;
   font-size: 2.7vh;
   font-family: Montaga;
   display: flex;
   align-items: center;
-  flex-direction: row;
-  margin-left: 5%;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 50px;
 `;
 
 const BookDiscovery = (props) => {
@@ -49,30 +53,26 @@ const BookDiscovery = (props) => {
   };
 
   return (
-    <div style={{ marginLeft: "5%", marginRight: "5%", marginBottom: "5%" }}>
-      <Title>Books</Title>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
-      >
-        <AddCard>
-          <div style={{ display: "flex" }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="40"
-              viewBox="0 0 40 40"
-              width="40"
-            >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
-            </svg>
-            <div style={{ fontFamily: "Poppins" }}>Submit a book</div>
-          </div>
-        </AddCard>
-        <div>
+    <>
+      <Helmet>
+        <title>Proofed - Books</title>
+      </Helmet>
+      <div style={{ marginLeft: "5%", marginRight: "5%", marginBottom: "5%" }}>
+        <HorizontalSpacer size={30} />
+        {/* Header and control */}
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Title>Books</Title>
+          <SubmitCard onClick={() => history.push(`/search-book`)}>
+            <PlusIcon />
+            <div style={{ fontFamily: "Poppins", marginLeft: "20px" }}>
+              Submit a Book Review
+            </div>
+          </SubmitCard>
+        </div>
+        {/* Spacer */}
+        <HorizontalSpacer size={40} />
+        {/* Grid */}
+        <Grid>
           {books?.map((book, i) => (
             <BookCard
               key={i}
@@ -84,9 +84,9 @@ const BookDiscovery = (props) => {
               rating={book.averageRatingOutOfFive}
             />
           ))}
-        </div>
+        </Grid>
       </div>
-    </div>
+    </>
   );
 };
 
